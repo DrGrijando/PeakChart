caaf636e22c3330e211e571071eedfdccd59d75e
Ruben Gomez <rubengp64@gmail.com>
2021-12-18 10:42:34 +0100

    Added sample Xamarin.Forms project

---
+++ FlowChart/FlowChart/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FlowChart.ViewModels;
+using FlowChart.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FlowChart
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ FlowChart/FlowChart/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FlowChart.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FlowChart.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
